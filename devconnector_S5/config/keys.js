6e052f61cea0a5dbcbcb0c27734fc23ed7f400b8
abhinandanchivate <abhinandanchivate2000@gmail.com>
2021-08-25 12:48:12 +0530

    Update keys.js

--- devconnector_S5/config/keys.js
+++ devconnector_S5/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:admin@cluster0.buysa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  mongoURI: "mongodb://admin:admin@cluster0-shard-00-00.buysa.mongodb.net:27017,cluster0-shard-00-01.buysa.mongodb.net:27017,cluster0-shard-00-02.buysa.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-95ikn1-shard-0&authSource=admin&retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
